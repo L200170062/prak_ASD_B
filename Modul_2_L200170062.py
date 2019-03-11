be66fbb258618eadc9a78eb10e49297a934ea4ea
Muhammad Huda fatoni <43444181+L200170062@users.noreply.github.com>
2019-03-11 22:46:41 +0700

    Add files via upload

--- Modul_2_L200170062.py
+++ Modul_2_L200170062.py
@@ -1,3 +1,8 @@
+##Sidiq Zainudin
+##L200170058
+##B
+
+
 ##Latihan
 
 class Manusia(object):
@@ -104,7 +109,7 @@ class Pesan(object):
                 kon += 1
         return kon
 
-##2
+##c
 
 class Pesan(object):
     """Sebuah class bernama Pesan.
@@ -146,54 +151,6 @@ class Pesan(object):
                 voc += 1
         return voc
     
-##C
-
-class Manusia(object):
-    """class 'Manusia' dengan inisiasi 'nama'"""
-    keadaan = "lapar"
-    def __init__(self, nama):
-        self.nama = nama
-    def ucapkanSalam(self):
-        print("Salam, namaku ",self.nama)
-    def makan(self, s):
-        print("Saya baru saja makan ", s)
-        self.keadaan = 'kenyang'
-    def olahraga(self, k):
-        print("Saya baru saja latihan ", k)
-        self.keadaan = 'lapar'
-    def mengalikanDenganDua(self, n):
-        return n * 2
-
-class Mahasiswa(Manusia):
-    """Class Mahasiswa yang dibangun dari class Manusia."""
-    def __init__(self, nama, NIM, kota, us):
-        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
-        self.nama = nama
-        self.NIM = NIM
-        self.kotaTinggal = kota
-        self.uangSaku = us
-    def __str__(self):
-        s = self.nama + ', NIM ' + str(self.NIM) \
-            + '. Tinggal di ' + self.kotaTinggal \
-            + ' tiap bulannya.'
-        return s
-    def ambilNama(self):
-        return self.nama
-    def ambilNIM(self):
-        return self.NIM
-    def ambilUangSaku(self):
-        return self.uangSaku
-    def makan(self, s):
-        """Metode ini menutupi metode 'makan'-nya class manusia.
-           Mahasiswa kalau makan sambil belajar."""
-        print("Saya baru saja makan", s, "Sambil belajar.")
-        self.keadaan = 'kenyang'
-
-    def ambilKotaTinggal(self):
-        return self.kotaTinggal
-    def perbaruiKotaTinggal(self, ubah):
-self.kotaTinggal = ubah
-
 ##2
 ##A
 
